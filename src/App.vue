7ee4a38a1a1c4aa3cfd8557eebcb673f1980cca3
yinzhuo <2572703497@qq.com>
2018-06-29 19:31:16 +0800

    在线翻译小demo

---
+++ src/App.vue
@@ -0,0 +1,47 @@
+<template>
+  <div id="app">
+    <h1>在线翻译</h1>
+    <h5 class="text-muted">简单 / 易用 / 便捷</h5>
+    <translateForm v-on:formSubmit="translateText"></translateForm>
+    <translateOutput v-text="translatedText"></translateOutput>
+  </div>
+</template>
+
+<script>
+
+import TranslateForm from './components/TranslateForm'
+import TranslateOutput from './components/TranslateOutput'
+
+export default {
+  name: 'app',
+  data:function(){
+    return{
+      translatedText:""
+    }
+  },
+  components: {
+    TranslateForm,TranslateOutput
+  },
+  methods:{
+    translateText:function(text,language){
+      // alert(text);
+      this.$http.get('https://translate.yandex.net/api/v1.5/tr.json/translate?key=trnsl.1.1.20180613T082725Z.d1ad0e2a01564d3a.d54d24a027df946e8fab34fc31da76b94875d0a5&lang='+language+'&text='+text)
+          .then((response)=>{
+            // console.log(response.body.text[0]);
+            this.translatedText = response.body.text[0];
+          })
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
